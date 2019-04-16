d3cd5a8d2dd197ede441dc902582accbacbe0c94
konkrer <rivortex@gmail.com>
2019-04-16 00:09:45 -0700

    django-framework viewset creaat, retrieve, update, partial_update, destroy

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -54,13 +54,47 @@ class HelloApiView(APIView):
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet."""
 
+    serializer_class = serializers.HelloSerializer
+
     def list(self, request):
         """Return Hello message"""
 
         a_viewset = [
-            'Users actions (list, create, retrieve, update, partial_update)',
+            'Users actions (list, create, retrieve, update, partial_update, destroy)',
             'Automatically maps to URLs using Routers',
             'Provides more functionality with less code.'
         ]
 
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hello message."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk):
+        """Get Object by ID."""
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk):
+        """Update object."""
+
+        return Response({'http_method':"PUT"})
+
+    def partial_update(self, request, pk=None):
+        """Update part of object."""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Delete object."""
+
+        return Response({'http_method': 'DELETE'})
